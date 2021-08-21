c46c083b2f3e2e6e056cdc318dcf68bea5080a29
MRUNAL <89269031+Mrunal1911@users.noreply.github.com>
2021-08-21 11:30:24 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://GitHub.com/ULTRA-OP/ULTRA-X && cd ULTRA-X && python3 -m ULTRA")
+system ("git clone https://GitHub.com/Mrunal1911/MrunalX && cd MrunalX && python3 -m ULTRA")
 # try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @Beast_SElling")
