315d6c102d9cc4ab6be4de86521785562f9f7c62
MRUNAL <89269031+Mrunal1911@users.noreply.github.com>
2021-08-21 16:05:58 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system 
-system ("git clone https://github.com/Mrunal1911/Mrunal-X.git && cd Mrunal-X && python3 -m userbot")
+system ("git clone https://github.com/Mrunal1911/AngelX.git && cd AngelX && python3 -m ULTRA")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @Beast_SElling")
