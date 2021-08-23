5b16ea51360e9b391b67f94fcbb85c37cd995faf
MRUNAL <89269031+Mrunal1911@users.noreply.github.com>
2021-08-23 13:04:51 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system 
-system ("git clone https://github.com/Mrunal1911/AngelX.git && cd AngelX && python3 -m ULTRA")
+system ("git clone https://github.com/Mrunal1911/xultraz.git && cd xultraz && python3 -m ULTRA")
 # try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @Beast_SElling")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @sinx_support")
